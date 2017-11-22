ccab3e37b2d48f60029182e53c523a7aaeddaf99
Sam Nyashev <ll2.lol@yandex.ru>
2017-11-23 01:29:41 +0300

    new version.

--- npc_item_drop.lua
+++ npc_item_drop.lua
@@ -2,22 +2,44 @@ PLUGIN.name = "NPC Item Drop"
 PLUGIN.author = "Hikka (NS 1.1)"
 PLUGIN.desc = "За убийство NPC, будет выпадать вещь"
 
-PLUGIN.chance = 100 -- шанс выпадение вещи
-PLUGIN.items = { -- вещи которые будут выпадать
-	"pistol",
-	"spraycan",
-}
-PLUGIN.typeNPC = { -- тип npc, с которого падает награда
-	"npc_bug",
-	"npc_zombie",
+PLUGIN.itemDrops = {
+	drop = {
+		{item = "pistol", chance = 100, npc = {"npc_zombie"}}, -- выпадаемый предмет; шанс выпадения с NPC; типы NPC с которых выпадет вещь.
+		{item = "spraycan", chance = 10, npc = {"npc_zombie", "npc_bug"}},
+		{item = "water", chance = 15, npc = {"npc_zombie", "npc_bug", "npc_fastzombie"}},
+	},
+	
+	money = {
+		enabled = true, -- Выпадение денег при убийстве NPC. (true/false)
+		amount = 100, -- Максимальная сумма выпадения денег.
+	},
 }
 
-function PLUGIN:OnNPCKilled(entity)
-	if math.random( 0, 100 ) <= math.Clamp( self.chance, 0, 100 ) then
-		for _, x in pairs(self.typeNPC) do
-			if entity:GetClass() == x then
-				nut.item.spawn(table.Random(self.items), entity:GetPos() + Vector(0, 0, 8))
+function PLUGIN:OnNPCKilled(entity, attacker)
+	if (!IsValid(entity)) then
+		return
+	end
+
+	if (!IsValid(attacker) and !attacker:IsPlayer()) then
+		return
+	end
+
+	local chnce, class = 100 * math.random(), entity:GetClass()
+	for _, data in ipairs(self.itemDrops) do
+		local dropTbl = data.drop
+		local moneyTbl = data.money
+		
+		if (class == dropTbl.npc) then
+			if (chnce > dropTbl.chance) then
+				break
 			end
+			
+			if (moneyTbl.enabled) then
+				nut.currency.spawn(entity:GetPos() + Vector(0, 0, 20), math.random(1, moneyTbl.amount or 100))
+			end
+			
+			nut.item.spawn(table.Random(dropTbl.item), entity:GetPos() + Vector(0, 0, 15))
 		end
+		break
 	end
 end
