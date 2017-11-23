442cc045d7a1a373d0f8d8d5274b970a0c4d18a1
Sam Nyashev <ll2.lol@yandex.ru>
2017-11-23 17:03:15 +0300

    Add files via upload

--- health_player.lua
+++ health_player.lua
@@ -1,3 +1,4 @@
+local PLUGIN = PLUGIN
 PLUGIN.name = "Health player"
 PLUGIN.author = "Hikka (NS 1.1)"
 PLUGIN.desc = "Saves the health of a character." -- Изменяет текущую систему ХП, т.е если игрок вышел с 50HP, то при заходе у него будет 50HP :D
@@ -9,40 +10,42 @@ function playerMeta:getHealth()
 	return (self:getNetVar(HealthID)) or 0
 end
 
-function PLUGIN:PlayerDeath(client)
-	if (!IsValid(client) and !client:IsPlayer()) then
-		return
+if (SERVER) then
+	function PLUGIN:PlayerDeath(client)
+		if (!IsValid(client) and !client:IsPlayer()) then
+			return
+		end
+
+		client.refillHealth = true
 	end
 
-	client.refillHealth = true
-end
-
-function PLUGIN:PlayerSpawn(client)
-	if (!IsValid(client) and !client:IsPlayer()) then
-		return
+	function PLUGIN:PlayerSpawn(client)
+		if (!IsValid(client) and !client:IsPlayer()) then
+			return
+		end
+		
+		if (client.refillHealth) then
+			local hpAmount = client:GetMaxHealth()
+			client:setNetVar(HealthID, hpAmount)
+			client.refillHealth = false
+		end
 	end
-	
-	if (client.refillHealth) then
+
+	function PLUGIN:CharacterPreSave(character)
+		local client = character.player
+		local savedHealth = client:getHealth()
+		local maxHealth = client:GetMaxHealth()
+		character:setData(HealthID, math.Clamp(savedHealth, 0, maxHealth))
+	end
+
+	function PLUGIN:PlayerLoadedChar(client, character)
+		local hpData = character:getData(HealthID)
 		local hpAmount = client:GetMaxHealth()
-		client:setNetVar(HealthID, hpAmount)
-		client.refillHealth = false
-	end
-end
-
-function PLUGIN:CharacterPreSave(character)
-	local client = character.player
-	local savedHealth = client:getHealth()
-	local maxHealth = client:GetMaxHealth()
-	character:setData(HealthID, math.Clamp(savedHealth, 0, maxHealth))
-end
-
-function PLUGIN:PlayerLoadedChar(client, character)
-	local hpData = character:getData(HealthID)
-	local hpAmount = client:GetMaxHealth()
-	if (hpData) then
-		client:setNetVar(HealthID, math.Clamp(hpData, 0, hpAmount))
-		client:SetHealth(math.Clamp(hpData, 0, hpAmount))
-	else
-		client:setNetVar(HealthID, hpAmount)
+		if (hpData) then
+			client:setNetVar(HealthID, math.Clamp(hpData, 0, hpAmount))
+			client:SetHealth(math.Clamp(hpData, 0, hpAmount))
+		else
+			client:setNetVar(HealthID, hpAmount)
+		end
 	end
 end
