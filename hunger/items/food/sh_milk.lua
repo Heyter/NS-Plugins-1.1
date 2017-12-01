20c84d4d29ac04a8ef7b0732bfb93cf56e4237be
Sam Nyashev <ll2.lol@yandex.ru>
2017-12-01 05:52:44 +0300

    Delete sh_milk.lua

--- hunger/items/food/sh_milk.lua
+++
@@ -1,7 +0,0 @@
-ITEM.name = "Молоко"
-ITEM.desc = "Картонная упоковка молока."
-ITEM.category = "Еда"
-ITEM.model = "models/props_junk/garbage_milkcarton002a.mdl"
-ITEM.hunger = 0
-ITEM.thirst = 25
-ITEM.empty = false
