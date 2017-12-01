ab00a0c6b0d153a2e679a859c081fb31d6d0c2e2
Sam Nyashev <ll2.lol@yandex.ru>
2017-12-01 05:53:29 +0300

    Delete sh_water.lua

--- hunger/items/food/sh_water.lua
+++
@@ -1,7 +0,0 @@
-ITEM.name = "Газировка"
-ITEM.desc = "Алюминивая банка с газировкой."
-ITEM.category = "Еда"
-ITEM.model = "models/props_junk/popcan01a.mdl"
-ITEM.hunger = 0
-ITEM.thirst = 15
-ITEM.empty = false
