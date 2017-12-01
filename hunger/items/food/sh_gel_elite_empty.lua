ea7020dfbfd33f6fec5db6341c545e49055386f3
Sam Nyashev <ll2.lol@yandex.ru>
2017-12-01 05:52:10 +0300

    Delete sh_gel_elite_empty.lua

--- hunger/items/food/sh_gel_elite_empty.lua
+++
@@ -1,8 +0,0 @@
-ITEM.name = "Банка"
-ITEM.desc = "Бальшая пустая банка."
-ITEM.category = "Еда"
-ITEM.model = "models/props_lab/jar01a.mdl"
-ITEM.hunger = 0
-ITEM.thirst = 0
-ITEM.empty = true
-ITEM.permit = "food"
