9ff3d888798c87399d8c627de9b0b1572c7358d1
Sam Nyashev <ll2.lol@yandex.ru>
2017-12-01 05:52:09 +0300

    Delete sh_gel_elite.lua

--- hunger/items/food/sh_gel_elite.lua
+++
@@ -1,8 +0,0 @@
-ITEM.name = "Элитный Гель"
-ITEM.desc = "Выдается работникам CWU и лоялистам."
-ITEM.category = "Еда"
-ITEM.model = "models/props_lab/jar01a.mdl"
-ITEM.hunger = 100
-ITEM.thirst = 20
-ITEM.empty = false
-ITEM.permit = "food"
