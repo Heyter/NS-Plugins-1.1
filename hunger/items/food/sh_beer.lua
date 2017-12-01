4dad9595b09f0b32a54f0fe2266c424c5a1a9261
Sam Nyashev <ll2.lol@yandex.ru>
2017-12-01 05:52:04 +0300

    Delete sh_beer.lua

--- hunger/items/food/sh_beer.lua
+++
@@ -1,7 +0,0 @@
-ITEM.name = "Пиво"
-ITEM.desc = "Стеклянная бутылка пива."
-ITEM.category = "Еда"
-ITEM.model = "models/props_junk/garbage_glassbottle001a.mdl"
-ITEM.hunger = 0
-ITEM.thirst = 15
-ITEM.empty = false
