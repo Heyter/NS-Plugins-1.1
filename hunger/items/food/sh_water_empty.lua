d03cd46d92dac8c482820d660970bdfdec8c1516
Sam Nyashev <ll2.lol@yandex.ru>
2017-12-01 05:51:41 +0300

    Delete sh_water_empty.lua

--- hunger/items/food/sh_water_empty.lua
+++
@@ -1,8 +0,0 @@
-ITEM.name = "Мусор(Для переработки)"
-ITEM.desc = "Алюминивая банка из под газировкой."
-ITEM.category = "Еда"
-ITEM.model = "models/props_junk/popcan01a.mdl"
-ITEM.hunger = 0
-ITEM.thirst = 0
-ITEM.empty = true
-ITEM.permit = "food"
