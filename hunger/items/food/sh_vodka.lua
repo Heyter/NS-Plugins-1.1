cec8b8000548ad46b2041043e6ef4069f68642fd
Sam Nyashev <ll2.lol@yandex.ru>
2017-12-01 05:53:11 +0300

    Delete sh_vodka.lua

--- hunger/items/food/sh_vodka.lua
+++
@@ -1,7 +0,0 @@
-ITEM.name = "Водка"
-ITEM.desc = "Стеклянная бутылка с прозрачной жидкостью."
-ITEM.category = "Еда"
-ITEM.model = "models/props_junk/GlassBottle01a.mdl"
-ITEM.hunger = 0
-ITEM.thirst = 15
-ITEM.empty = false
