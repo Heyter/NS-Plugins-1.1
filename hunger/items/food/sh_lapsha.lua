f866784655483788025db8d6a717f34499f17a8d
Sam Nyashev <ll2.lol@yandex.ru>
2017-12-01 05:52:30 +0300

    Delete sh_lapsha.lua

--- hunger/items/food/sh_lapsha.lua
+++
@@ -1,8 +0,0 @@
-ITEM.name = "Лапша"
-ITEM.desc = "Китайская лапша, более съедобна чем гель."
-ITEM.category = "Еда"
-ITEM.model = "models/props_junk/garbage_takeoutcarton001a.mdl"
-ITEM.hunger = 40
-ITEM.thirst = 0
-ITEM.empty = false
-ITEM.permit = "food"
