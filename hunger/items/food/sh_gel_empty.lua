56c585e6ebd72b00fc3448d1985e91d096cfb74f
Sam Nyashev <ll2.lol@yandex.ru>
2017-12-01 05:52:12 +0300

    Delete sh_gel_empty.lua

--- hunger/items/food/sh_gel_empty.lua
+++
@@ -1,8 +0,0 @@
-ITEM.name = "Мусор(Для переработки)"
-ITEM.desc = "Пустая пластиковая банка из под геля."
-ITEM.category = "Еда"
-ITEM.model = "models/props_lab/jar01b.mdl"
-ITEM.hunger = 50
-ITEM.thirst = 0
-ITEM.empty = true
-ITEM.permit = "food"
