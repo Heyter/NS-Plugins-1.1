4b226f274715f27771408c0655e25e4abd7136e4
Sam Nyashev <ll2.lol@yandex.ru>
2017-12-01 05:52:48 +0300

    Delete sh_sok.lua

--- hunger/items/food/sh_sok.lua
+++
@@ -1,7 +0,0 @@
-ITEM.name = "Сок"
-ITEM.desc = "3 литра сока."
-ITEM.category = "Еда"
-ITEM.model = "models/props_junk/garbage_plasticbottle001a.mdl"
-ITEM.hunger = 0
-ITEM.thirst = 70
-ITEM.empty = false
