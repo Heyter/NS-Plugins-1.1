416ac8ae3fdeb2fdce497fc875010415df170a84
Sam Nyashev <ll2.lol@yandex.ru>
2017-12-01 05:51:37 +0300

    Delete sh_water_sparkling_empty.lua

--- hunger/items/food/sh_water_sparkling_empty.lua
+++
@@ -1,9 +0,0 @@
-ITEM.name = "Мусор"
-ITEM.desc = "Пустая Алюминивая Банка."
-ITEM.model = "models/props_junk/popcan01a.mdl"
-ITEM.category = "Еда"
-ITEM.skin = 2
-ITEM.hunger = 0
-ITEM.thirst = 0
-ITEM.empty = true
-ITEM.permit = "food"
