eec1995ded2922d40a52de82f16a9dc5f58d2c6d
Sam Nyashev <ll2.lol@yandex.ru>
2017-12-01 05:53:10 +0300

    Delete sh_soup_empty.lua

--- hunger/items/food/sh_soup_empty.lua
+++
@@ -1,8 +0,0 @@
-ITEM.name = "Консерва"
-ITEM.desc = "Старая консерва из под супа."
-ITEM.category = "Еда"
-ITEM.model = "models/props_junk/garbage_metalcan001a.mdl"
-ITEM.hunger = 0
-ITEM.thirst = 0
-ITEM.empty = true
-ITEM.permit = "food"
